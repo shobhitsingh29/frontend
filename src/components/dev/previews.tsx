b5c0ed00387c6fdc6e29096118fcb5d15cbe631e
shosingh <shobhit.singh@zalando.de>
2023-05-11 01:55:13 +0200

    add

---
+++ src/components/dev/previews.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentPreview, Previews } from '@react-buddy/ide-toolbox-next';
+import { PaletteTree } from './palette';
+import IndexPage from '~/pages';
+
+const ComponentPreviews = () => {
+  return (
+    <Previews palette={<PaletteTree />}>
+      <ComponentPreview path='/IndexPage'>
+        <IndexPage />
+      </ComponentPreview>
+    </Previews>
+  );
+};
+
+export default ComponentPreviews;
